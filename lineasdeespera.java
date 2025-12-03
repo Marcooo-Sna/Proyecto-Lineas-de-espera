dbb642898bb2faf9e22c3a20c446f1784a2ce581
Marcooo-Sna <mjeysiona13@gmail.com>
2025-12-03 16:32:56 -0600

    menu

--- lineasdeespera.java
+++ lineasdeespera.java
@@ -1,13 +1,20 @@
-package Proyecto
-
 import java.util.Scanner;
-
--Lineas-de-espera;
-
 public class lineasdeespera {
     public static void main(String [] args);{
         Scanner TSEscritora = new Scanner(System.in);
-        
+        do{
+            System.out.println("Modelo de colas.");
+            System.out.println("1. Modelo M/M/1.");
+            System.out.println("2. Modelo M/M/C.");
+            System.out.println("3. Modelo M/M/1/K.");
+            System.out.println("4. Salir.");
+
+            switch (opcion) {
+            }
+
+            }
+        }
+
     }
     
 }
