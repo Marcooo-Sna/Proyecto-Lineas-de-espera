82488c2fffd9480597945383bbfb95c2d674cc01
Marcooo-Sna <mjeysiona13@gmail.com>
2025-12-03 16:16:36 -0600

    Empezar

---
+++ lineasdeespera.java
@@ -0,0 +1,13 @@
+package Proyecto
+
+import java.util.Scanner;
+
+-Lineas-de-espera;
+
+public class lineasdeespera {
+    public static void main(String [] args);{
+        Scanner TSEscritora = new Scanner(System.in);
+        
+    }
+    
+}
